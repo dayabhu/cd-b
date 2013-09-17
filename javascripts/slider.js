7654af7a162653d613fa6008087d3ad27a8f3cba
dayabhu <dayasmiles@gmail.com>
2013-09-17 14:38:57 +0100

    intial update

---
+++ javascripts/slider.js
@@ -0,0 +1,51 @@
+ $(document).ready(function (){
+
+       $('#slider1').orbit({
+       // animation: 'horizontal-slide',                  // fade, horizontal-slide, vertical-slide, horizontal-push
+       // animationSpeed: 800,                // how fast animtions are
+       timer: false,                        // true or false to have the timer
+       // //resetTimerOnClick: false,           // true resets the timer instead of pausing slideshow progress
+       // //advanceSpeed: 4000,                 // if timer is enabled, time between transitions
+       // pauseOnHover: false,                // if you hover pauses the slider
+       // startClockOnMouseOut: false,        // if clock should start on MouseOut
+       // //startClockOnMouseOutAfter: 1000,    // how long after MouseOut should the timer start again
+       // directionalNav: true,               // manual advancing directional navs
+       // //captions: true,                     // do you want captions?
+       // //captionAnimation: 'none',           // fade, slideOpen, none
+       // //captionAnimationSpeed: 800,         // if so how quickly should they animate in
+       // bullets: true,                     // true or false to activate the bullet navigation
+       // bulletThumbs: false,                // thumbnails for the bullets
+       // bulletThumbLocation: '',            // location from this file where thumbs will be
+       // afterSlideChange: function(){},   // empty function
+       // fluid: true                         // or set a aspect ratio for content slides (ex: '4x3')
+});
+ $('#slider2').orbit({
+  // animation: 'horizontal-slide',                  // fade, horizontal-slide, vertical-slide, horizontal-push
+  // animationSpeed: 800,                // how fast animtions are
+  timer: false,                        // true or false to have the timer
+  // //resetTimerOnClick: false,           // true resets the timer instead of pausing slideshow progress
+  // //advanceSpeed: 4000,                 // if timer is enabled, time between transitions
+  // pauseOnHover: false,                // if you hover pauses the slider
+  // startClockOnMouseOut: false,        // if clock should start on MouseOut
+  // //startClockOnMouseOutAfter: 1000,    // how long after MouseOut should the timer start again
+  // directionalNav: true,               // manual advancing directional navs
+  // //captions: true,                     // do you want captions?
+  // //captionAnimation: 'none',           // fade, slideOpen, none
+  // //captionAnimationSpeed: 800,         // if so how quickly should they animate in
+  // bullets: true,                     // true or false to activate the bullet navigation
+  // bulletThumbs: false,                // thumbnails for the bullets
+  // bulletThumbLocation: '',            // location from this file where thumbs will be
+  // // afterSlideChange: function(){},     // empty function
+  // // fluid: true                         // or set a aspect ratio for content slides (ex: '4x3')
+});
+
+
+
+ });
+   
+
+
+
+
+
+
---
+++ javascripts/sliderone.js
@@ -0,0 +1,41 @@
+    $(document).ready(function (){
+          var fromSlide1 = false;
+          var fromSlide2 = false;
+          var slider1 = $("#slider1");
+          var slider2 = $("#slider2");
+
+
+          var slider1Prev = slider1.find('.right');
+          console.log(slider1Prev);
+          // var slider2Prev = slider2.find('.orbit.prev');
+          // var slider1Next = slider1.find(".orbit.next");
+          // var slider2Next = slider2.find(".orbit.next");
+
+          // slider1Prev.click(function () {
+          //     if (fromSlide1) return;
+          //     fromSlide1 = true;              
+          //     slider2Prev.click();
+          //     fromSlide1 = false;
+          // });
+          // slider2Prev.click(function () {
+          //     if (fromSlide2) return;
+          //     fromSlide2 = true;
+          //     slider1Prev.click();
+          //     fromSlide2 = false;
+          // });
+          // slider1Next.click(function () {
+          //     if (fromSlide1) return;
+          //     fromSlide1 = true;              
+          //     slider2Next.click();
+          //     fromSlide1 = false;
+          // });
+          // slider2Next.click(function () {
+          //     if (fromSlide2) return;
+          //     fromSlide2 = true;
+          //     slider1Next.click();
+          //     fromSlide2 = false;
+          // });
+
+
+
+ });
